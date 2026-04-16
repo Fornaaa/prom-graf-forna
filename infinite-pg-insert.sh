6fa52c10b85ca8669cfb80b356a12cdd01dc3d91
Fornaaa <agustinfornari7@gmail.com>
2026-04-15 21:58:46 -0300

    hola

--- infinite-pg-insert.sh
+++ infinite-pg-insert.sh
@@ -7,7 +7,7 @@
 while [ true ]; do
     echo Insert new user in db
     #psql postgres://workshop:workshop@localhost:5432/workshop --command "INSERT INTO users(email, password, name) VALUES ('a', 'b', 'c');"
-    docker-compose exec postgres psql postgres://workshop:workshop@localhost:5432/workshop --command "INSERT INTO users(email, password, name) VALUES ('a', 'b', 'c');"
+    docker compose exec postgres psql postgres://workshop:workshop@localhost:5432/workshop --command "INSERT INTO users(email, password, name) VALUES ('a', 'b', 'c');"
 
     # run at least once every 30 seconds
     pause_time=$(( ${RANDOM} % 30 ))
